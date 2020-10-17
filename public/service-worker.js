218ba29b3a924d2aab93bc1064a17a95db444c5d
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-16 20:16:28 -0400

    Initial Commit

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+var urlsToCache = [
+  "/",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
+
+self.addEventListener("install", function(event) {
+  // Perform install steps
+ 
+});
+
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+ 
+});
